69be535d1ce634da38d1e069d35ce362874eb12a
Arthur <arturshatailo@gmail.com>
2023-01-06 01:55:38 +0200

    card details crud controller development

--- payment_methods_management/src/main/java/com/exchange/payment_methods_management/controller/CardDetailsController.java
+++ payment_methods_management/src/main/java/com/exchange/payment_methods_management/controller/CardDetailsController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping(value = "/api/p-methods", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/api/p-methods/c", produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
 public class CardDetailsController {
 
@@ -20,10 +20,29 @@ public class CardDetailsController {
 
     private final DetailsCrudService<CardDetails, Long> cardDetailsService;
 
-    @PostMapping(value = "/c")
+    @PostMapping(value = "/")
     @ResponseStatus(HttpStatus.CREATED)
     public void addCardDetails(@RequestBody CardDetailsDTO dto){
         cardDetailsService.add(cardDetailsMapper.cardDetailsDTOtoObject(dto));
     }
+
+    @PutMapping(value = "/", params = {"id"})
+    @ResponseStatus(HttpStatus.OK)
+    public void updateCardDetails(@RequestBody CardDetailsDTO dto, @RequestParam Long id) {
+        cardDetailsService.update(cardDetailsMapper.cardDetailsDTOtoObject(dto), id);
+    }
+
+    @DeleteMapping(value = "/" , params = {"id", "email"})
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteCardDetails(@RequestParam Long id, @RequestParam String email) {
+        cardDetailsService.delete(id, email);
+    }
+
+    @GetMapping(value = "/", params = {"id", "email"})
+    @ResponseStatus(HttpStatus.FOUND)
+    public CardDetails getCardDetails(@RequestParam Long id, @RequestParam String email) {
+        return cardDetailsService.get(id, email);
+    }
+
 
 }
